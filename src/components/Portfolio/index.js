458aefabe9214e5a6065e5a9654d7f1db5510b30
Rodrigo Godoy <contato@rodrigogodoy.net>
2020-07-13 16:17:10 -0300

    mostrando o modal de portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,6 +4,8 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 
 import PortfolioJson from '../../assets/portfolio.json'
 
+import BotaoFechar from '../../assets/imagens/portfolio/fechar.svg'
+
 import 'swiper/components/navigation/navigation.scss'
 import 'swiper/swiper.scss';
 import './portfolio.scss';
@@ -21,23 +23,47 @@ const CardPortfolio = (props) => {
   )
 }
 export default class Portfolio extends Component {
+  state = {
+    modal: false,
+    tituloModal: ''
+  }
 
+  constructor() {
+    super()
+    this.abreModal = this.abreModal.bind(this);
+  }
+  abreModal(conteudo){
+    this.setState({
+      modal: !this.state.modal,
+      tituloModal: conteudo.titulo
+    })
+  }
   render() {
 
     return (
-
-    <Swiper
-      spaceBetween={0}
-      slidesPerView={5}
-      navigation
-    >
-      { PortfolioJson.map( (valor, index) => (
-        <SwiperSlide key = { index } >
-            <CardPortfolio titulo = { valor.titulo } thumb = { valor.thumb } />
-        </SwiperSlide>
-      ) ) }
-    </Swiper>
-
+      <div className = 'container container--full'>
+        {this.state.modal &&
+          <div className = 'modal-portfolio'>
+            <div className = 'modal-portfolio__conteudo'>
+              <button onClick = { () => { this.abreModal('') }} className = 'modal-portfolio__fechar'>
+                <img src = { BotaoFechar } alt = 'Fechar' />
+              </button>
+              <h3 className = 'modal-portfolio__titulo'> { this.state.tituloModal } </h3>
+            </div>
+          </div>
+        }
+        <Swiper
+          spaceBetween={0}
+          slidesPerView={5}
+          navigation
+        >
+          { PortfolioJson.map( (valor, index) => (
+            <SwiperSlide key = { index }  onClick = { () => this.abreModal(valor.conteudo) }>
+                <CardPortfolio titulo = { valor.titulo } thumb = { valor.thumb } />
+            </SwiperSlide>
+          ) ) }
+        </Swiper>
+      </div>
     )
 
   }
