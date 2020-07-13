fbd5fc0c9fe2c921cd184af71b2f1783d7b8f9aa
Rodrigo Godoy <contato@rodrigogodoy.net>
2020-07-13 11:39:51 -0300

    portfolio com o swipper

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import PortfolioJson from '../../assets/portfolio.json'
+
+import 'swiper/components/navigation/navigation.scss'
 import 'swiper/swiper.scss';
 import './portfolio.scss';
 
-const CardPortfolio = () => {
+SwiperCore.use([Navigation]);
+
+const CardPortfolio = (props) => {
   return (
     <div className = 'card-portfolio'>
-      <p style = {{padding: 0, margin: 0}}>teste</p>
+      <div className = 'card-portfolio__conteudo'>
+        <p className = 'card-portfolio__texto'> { props.titulo } </p>
+      </div>
+      <img className = 'card-portfolio__imagem' src = { props.thumb } alt = { props.titulo } />
     </div>
   )
 }
@@ -19,10 +29,13 @@ export default class Portfolio extends Component {
     <Swiper
       spaceBetween={0}
       slidesPerView={5}
+      navigation
     >
-      <SwiperSlide>
-        <CardPortfolio />
-      </SwiperSlide>
+      { PortfolioJson.map( (valor, index) => (
+        <SwiperSlide key = { index } >
+            <CardPortfolio titulo = { valor.titulo } thumb = { valor.thumb } />
+        </SwiperSlide>
+      ) ) }
     </Swiper>
 
     )
